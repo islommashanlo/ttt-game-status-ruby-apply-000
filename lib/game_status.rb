01dff3bb426989fde805938a5c3c475da69ffd81
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-15 15:40:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,24 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+def won?(board)
+  if board.all? {|value| value == " "} or full_board(board)
+    return false
+  else
+  WIN_COMBINATIONS.any?{|combo|
+    combo.all?{|input|
+        board[input] == "X" or board[input] == "O"
+      }
+  end
+  }
+end
